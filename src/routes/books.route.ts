033f6540b7c33c51629bb60f55e386b5784861ad
Ariandi Nugraha <db_duabelas@yahoo.com>
2026-02-28 10:19:36 +0700

    remove authentication middleware from GET /books route

--- src/routes/books.route.ts
+++ src/routes/books.route.ts
@@ -7,12 +7,9 @@ import {
   getBooks,
   updateBook,
 } from "../controllers/books.controller";
-import { authMiddleware } from "../middleware/auth.middleware";
-
 const router = Router();
 
-// Level 5: GET /books is protected with auth
-router.get("/", authMiddleware, getBooks);
+router.get("/", getBooks);
 router.get("/:id", getBookById);
 router.post("/", createBook);
 router.put("/:id", updateBook);
